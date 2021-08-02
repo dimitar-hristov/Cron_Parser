c0ca437b47d0bada7e4e76c56e71811cc2d88338
dimitar-hristov <dimitarshr@gmail.com>
2021-08-02 20:07:22 +0100

    Validation

--- expression_type.py
+++ expression_type.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 class ExpressionType(Enum):
-    MINUTES = 1
-    HOURS = 2
-    DAY_OF_MONTH = 3
-    MONTH = 4
-    DAY_OF_WEEK = 5
+    MINUTES = "minutes"
+    HOURS = "hours"
+    DAY_OF_MONTH = "day of month"
+    MONTH = "month"
+    DAY_OF_WEEK = "day of week"
  
--- main.py
+++ main.py
@@ -1,71 +1,106 @@
 import sys
 from expression_type import ExpressionType
+import re
 
 months = {"JAN": 1, "FEB": 2, "MAR": 3,"APR": 4,"MAY": 5,"JUN": 6,"JUL": 7,"AUG": 8,"SEP": 9,"OCT": 10,"NOV": 11,"DEC": 12}
+order_of_args = [ExpressionType.MINUTES, ExpressionType.HOURS, ExpressionType.DAY_OF_MONTH, ExpressionType.MONTH, ExpressionType.DAY_OF_WEEK]
+args_min_and_max_values = {
+    ExpressionType.MINUTES: (0, 59), 
+    ExpressionType.HOURS: (0, 23), 
+    ExpressionType.DAY_OF_MONTH: (1, 31), 
+    ExpressionType.MONTH: (1, 12), 
+    ExpressionType.DAY_OF_WEEK: (0, 6)
+}
 
-def process_date(date_expression: str, type: ExpressionType) -> str:
-    # Inputs:
-    #   *
-    #   */15
-    #   1-10
-    #   1,2,3,4,5,25
-    #   20
-
-    if type is ExpressionType.MINUTES:
-        min_value = 0
-        max_value = 60
-    elif type is ExpressionType.HOURS:
-        min_value = 0
-        max_value = 24
-    elif type == ExpressionType.DAY_OF_MONTH:
-        min_value = 1
-        max_value = 32
-    elif type == ExpressionType.MONTH:
-        min_value = 1
-        max_value = 13
-    elif type == ExpressionType.DAY_OF_WEEK:
-        min_value = 0
-        max_value = 7
-
-    if date_expression == "*":
-        output_date_list = [str(minute) for minute in range(min_value, max_value)]
+def process_expression(expression: str, min_value: int, max_value: int) -> str:
+    # *
+    if re.search("^\*$", expression):
+        output_date_list = [str(minute) for minute in range(min_value, max_value+1)]
         output_date = " ".join(output_date_list).strip()
-    elif "*/" in date_expression:
-        interval = int(date_expression.split("/")[1])
-        output_date_list = [str(minute) for minute in range(min_value, max_value, interval)]
+    # */15
+    elif re.search("^\*\/[0-9][0-9]?$", expression):
+        interval = int(expression.split("/")[1])
+        output_date_list = [str(minute) for minute in range(min_value, max_value+1, interval)]
         output_date = " ".join(output_date_list).strip()
-    elif "-" in date_expression:
-        start = int(date_expression.split("-")[0])
-        end = int(date_expression.split("-")[1])
+    # 1-10
+    elif re.search("^[0-9]-[0-9]$", expression):
+        start = int(expression.split("-")[0])
+        end = int(expression.split("-")[1])
         output_date_list = [str(minute) for minute in range(start, end+1)]
         output_date = " ".join(output_date_list).strip()
-    elif "," in date_expression:
-        output_date = date_expression
+    # 1,2,3,4,5,25
+    # 20
     else:
-        output_date = date_expression
+        output_date = expression
 
     return output_date
 
+def is_syntax_valid(expression: str):
+    regular_expressions = [
+        "^\*$",                 # *             
+        "^\*\/[0-9][0-9]?$",    # */15        
+        "^[0-9]-[0-9]$",        # 1-10        
+        "^[0-9](,[0-9]+)*$",    # 1,2,3,4,5,25
+        "^[0-9]+$"              # 20          
+    ]
+    for regex in regular_expressions:
+        if re.search(regex, expression):
+            return True
+    
+    return False
+
+def values_are_in_range(expression: str, min_value: int, max_value: int):
+    # *
+    if re.search("^\*$", expression):
+        return True
+    # */15
+    elif re.search("^\*\/[0-9][0-9]?$", expression):
+        value = expression.split("/")[1]
+        if int(value) < min_value or int(value) > max_value:
+            return False
+    # 1-10
+    elif re.search("^[0-9]-[0-9]$", expression):
+        values = expression.split("-")
+        for value in values:
+            if int(value) < min_value or int(value) > max_value:
+                return False
+    # 1,2,3,4,5,25
+    elif re.search("^[0-9](,[0-9]+)*$", expression):
+        values = expression.split(",")
+        for value in values:
+            if int(value) < min_value or int(value) > max_value:
+                return False
+    # 20 
+    else:
+        value = int(expression)
+        if value < min_value or value > max_value:
+            return False
+    
+    return True
+    
+
+
 cron_expression = sys.argv[1].split(" ")
 
-minutes_expression = cron_expression[0].strip()
-output_minutes = process_date(date_expression=minutes_expression, type=ExpressionType.MINUTES)
+if len(cron_expression) == 6:
+    output = []
+    for i in range(5):
+        expression = cron_expression[i].strip()
+        min_value = args_min_and_max_values.get(order_of_args[i])[0]
+        max_value = args_min_and_max_values.get(order_of_args[i])[1]
+        
+        if is_syntax_valid(expression=expression) and values_are_in_range(expression=expression, min_value=min_value, max_value=max_value):
+            output.append(process_expression(expression=expression, min_value=min_value, max_value=max_value))
+        else:
+            raise ValueError(f"Argument for `{str(order_of_args[i].value)}` has invalid value!")
+    # add the command at the end
+    output.append(cron_expression[5].strip())
 
-hours_expression = cron_expression[1].strip()
-output_hours = process_date(date_expression=hours_expression, type=ExpressionType.HOURS)
-
-day_of_moth_expression = cron_expression[2].strip()
-output_day_of_month = process_date(date_expression=day_of_moth_expression, type=ExpressionType.DAY_OF_MONTH)
-
-month_expression = cron_expression[3].strip()
-output_month = process_date(date_expression=month_expression, type=ExpressionType.MONTH)
-
-day_of_week_expression = cron_expression[4].strip()
-output_day_of_week = process_date(date_expression=day_of_week_expression, type=ExpressionType.DAY_OF_WEEK)
-
-print(f"{'minute'.ljust(14)}{output_minutes}")
-print(f"{'hour'.ljust(14)}{output_hours}")
-print(f"{'day of month'.ljust(14)}{output_day_of_month}")
-print(f"{'month'.ljust(14)}{output_month}")
-print(f"{'day of week'.ljust(14)}{output_day_of_week}")
-print(f"{'command'.ljust(14)}{cron_expression[5].strip()}")
+    print(f"{'minute'.ljust(14)}{output[0]}")
+    print(f"{'hour'.ljust(14)}{output[1]}")
+    print(f"{'day of month'.ljust(14)}{output[2]}")
+    print(f"{'month'.ljust(14)}{output[3]}")
+    print(f"{'day of week'.ljust(14)}{output[4]}")
+    print(f"{'command'.ljust(14)}{output[5]}")
+else:
+    print("Invalid input!")
