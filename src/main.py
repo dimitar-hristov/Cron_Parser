cc008c0dacaa45b42bfd9384cd5944e5eb6da481
dimitar-hristov <dimitarshr@gmail.com>
2021-08-03 21:32:14 +0100

    Add tests for main

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import sys
 
 from utils import utils
-from utils.const import ARGS_MIN_AND_MAX_VALUES, ORDER_OF_ARGS
+from utils import const
 
 
-def main():
+def main() -> str:
     # If no argument is provided or there is a missing element, print a help message
     if len(sys.argv) == 2 and len(sys.argv[1].split(" ")) == 6:
         cron_expression = sys.argv[1].split(" ")
         output = []
         for i in range(5):
             expression = cron_expression[i].strip()
-            min_value = ARGS_MIN_AND_MAX_VALUES.get(ORDER_OF_ARGS[i])[0]
-            max_value = ARGS_MIN_AND_MAX_VALUES.get(ORDER_OF_ARGS[i])[1]
+            min_value = const.ARGS_MIN_AND_MAX_VALUES.get(const.ORDER_OF_ARGS[i])[0]
+            max_value = const.ARGS_MIN_AND_MAX_VALUES.get(const.ORDER_OF_ARGS[i])[1]
 
             if utils.is_syntax_valid(
                 expression=expression
@@ -28,19 +28,21 @@ def main():
                 )
             else:
                 raise ValueError(
-                    f"Argument for `{str(ORDER_OF_ARGS[i].value)}` has invalid value!"
+                    f"Argument for `{str(const.ORDER_OF_ARGS[i].value)}` has invalid value!"
                 )
         # add the command at the end
         output.append(cron_expression[5].strip())
 
-        print(f"{'minute'.ljust(14)}{output[0]}")
-        print(f"{'hour'.ljust(14)}{output[1]}")
-        print(f"{'day of month'.ljust(14)}{output[2]}")
-        print(f"{'month'.ljust(14)}{output[3]}")
-        print(f"{'day of week'.ljust(14)}{output[4]}")
-        print(f"{'command'.ljust(14)}{output[5]}")
+        return (
+            f"{'minute'.ljust(14)}{output[0]}\n"
+            f"{'hour'.ljust(14)}{output[1]}\n"
+            f"{'day of month'.ljust(14)}{output[2]}\n"
+            f"{'month'.ljust(14)}{output[3]}\n"
+            f"{'day of week'.ljust(14)}{output[4]}\n"
+            f"{'command'.ljust(14)}{output[5]}"
+        )
     else:
-        print(
+        return(
             "Cron Expression Parser. \n"
             "You need to provide a single string specifying: \n"
             "\t- minutes; \n"
@@ -54,6 +56,7 @@ def main():
 
 if __name__ == "__main__":
     try:
-        main()
+        result = main()
+        print(result)
     except Exception as e:
         print(e)
---
+++ tests/test_main.py
@@ -0,0 +1,82 @@
+from unittest.mock import patch
+from src.main import main
+import sys
+import pytest
+from utils import const
+
+@pytest.mark.parametrize(
+    "input",
+    [
+        (["script", "foo"]),
+        (["script"]),
+        (["script", "foo bar baz x y"]),
+        (["script", "foo bar baz x y z a"]),
+        (["script", "foo,bar,baz,x,y,z"]),
+    ],
+)
+def test_main_missing_args(input):
+    mock_args = input
+    with patch.object(sys, 'argv', mock_args):
+        actual = main()
+        expcted = "Cron Expression Parser. \n"\
+                "You need to provide a single string specifying: \n"\
+                "\t- minutes; \n"\
+                "\t- hours \n"\
+                "\t- day of month \n"\
+                "\t- month \n"\
+                "\t- day of week; \n"\
+                "\t- and the command as a cron expression."
+        assert actual == expcted
+
+def test_main_invalid_syntax(mocker):
+    mocker.patch.object(sys, "argv", ["script", "foo bar baz x y command"])
+    mock_arg = mocker.Mock()
+    mock_arg.value = "mock_args"
+    mocker.patch.object(const, "ORDER_OF_ARGS", [mock_arg])
+    mock_min_max = mocker.patch("main.const.ARGS_MIN_AND_MAX_VALUES")
+    mock_min_max.get.return_value = ["mock_min_value", "mock_max_value"]
+    mocker.patch("main.utils.is_syntax_valid", return_value = False)
+    
+    with pytest.raises(ValueError) as e:
+            main()
+    expcted_error = "Argument for `mock_args` has invalid value!"
+    
+    assert str(e.value) == expcted_error
+
+def test_main_value_not_in_range(mocker):
+    mocker.patch.object(sys, "argv", ["script", "foo bar baz x y command"])
+    mock_arg = mocker.Mock()
+    mock_arg.value = "mock_args"
+    mocker.patch.object(const, "ORDER_OF_ARGS", [mock_arg])
+    mock_min_max = mocker.patch("main.const.ARGS_MIN_AND_MAX_VALUES")
+    mock_min_max.get.return_value = ["mock_min_value", "mock_max_value"]
+    mocker.patch("main.utils.is_syntax_valid", return_value = True)
+    mocker.patch("main.utils.values_are_in_range", return_value = False)
+    
+    with pytest.raises(ValueError) as e:
+            main()
+    expcted_error = "Argument for `mock_args` has invalid value!"
+    
+    assert str(e.value) == expcted_error
+
+def test_main_success(mocker):
+    mocker.patch.object(sys, "argv", ["script", "foo bar baz x y command"])
+    mock_arg = mocker.Mock()
+    mock_arg.value = "mock_args"
+    mocker.patch.object(const, "ORDER_OF_ARGS", side_effect = mock_arg)
+    mock_min_max = mocker.patch("main.const.ARGS_MIN_AND_MAX_VALUES")
+    mock_min_max.get.return_value = ["mock_min_value", "mock_max_value"]
+    mocker.patch("main.utils.is_syntax_valid", return_value = True)
+    mocker.patch("main.utils.values_are_in_range", return_value = True)
+    mock_proccess_expression = mocker.patch("main.utils.process_expression")
+    mock_proccess_expression.side_effect = ["a", "b", "c", "d", "e"]
+    
+    actual_output = main()
+    expected_output = f"{'minute'.ljust(14)}a\n" \
+            f"{'hour'.ljust(14)}b\n" \
+            f"{'day of month'.ljust(14)}c\n" \
+            f"{'month'.ljust(14)}d\n" \
+            f"{'day of week'.ljust(14)}e\n" \
+            f"{'command'.ljust(14)}command"
+    
+    assert actual_output == expected_output
